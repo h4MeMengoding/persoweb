4504110566827e155f504d5a716986d29de77ad7
h4MeMengoding <ilhamshofaa@gmail.com>
2024-02-10 22:19:44 +0700

    gooooo

---
+++ src/components/illustrations/AboutMe.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+// eslint-disable-next-line no-unused-vars
+export default function AboutMeIllustration({ ...props }) {
+    const [isOpen, setOpenState] = useState(false);
+
+    useEffect(() => {
+        const section = document.getElementById("about");
+
+        window.addEventListener("scroll", () => {
+            if (section && window.pageYOffset >= section.offsetHeight - section.offsetTop - 50) {
+                setOpenState(true);
+            } else {
+                setOpenState(false);
+            }
+        });
+    });
+
+    if (!isOpen) {
+        return <section className="w-full max-w-[589px] h-screen max-h-72 md:max-h-[605px]" />;
+    } else {
+        return (
+            <>
+                <img data-aos="pulse" width={400} src="/images/me.jpg" alt="megtg" className="img-responsive" />
+            </>
+        );
+    }
+}
